b47d8bd2d94688a4babf08a4fb79e8e9ce306748
manaswini-k10 <manaswinikuchur@gmail.com>
2025-07-31 14:52:31 +0530

    fourth commit

--- sample.java
+++ sample.java
@@ -1,5 +1,7 @@
 class sample{
       public static void main(String[] args){
-                System.out.println("Hello World");
+            System.out.println("Hello World");
+            System.out.println("SCM polling to monitor change commits in github !!");
+            
       }
 } 
